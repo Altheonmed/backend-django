738164c6ba1f38abc8cc0c244d66aec5f6699b67
Omar NDIOUM <omar.ndioum.sn@gmail.com>
2026-01-07 11:36:53 +0000

    Sauvegarde des configurations de settings.py

--- telemedicine_project/settings.py
+++ telemedicine_project/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-ql$!*8icncxoj^a@hf()(5=xb=6h$iwt+mkx1c0par(%_r*lr)'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # ---
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
